ab3d43318ebb39603f18240b4c1f0768b2aa414a
Aaron Davis <108478438+arndvs@users.noreply.github.com>
2022-07-01 16:41:56 -0700

    Adding Items

---
+++ src/containers/NewItemFormContainer.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import { NewItemForm } from "../components/NewItemForm";
+import { addNewItem } from "../store/items/actions";
+
+const mapDispatchToProps = {
+    onSubmit: addNewItem,
+  };
+
+  export const NewItemFormContainer = connect(
+    null,
+    mapDispatchToProps
+  )(NewItemForm);
