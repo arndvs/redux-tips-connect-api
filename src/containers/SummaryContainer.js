761ff3bab6230ad478f6e3bdf7f9c8263a8aa46f
Aaron Davis <108478438+arndvs@users.noreply.github.com>
2022-07-02 13:49:25 -0700

    Deriving Data with Selectors - move calculations to selectors.js

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -1,22 +1,26 @@
 import { connect } from 'react-redux';
 import { Summary } from '../components/Summary';
+import { selectSubtotal, selectTipAmount, selectTotal } from '../store/items/selectors';
+
 
 const mapDispatchToProps = (state) => {
-  const items = state.items;
 
-  const subtotal = items.reduce(
-    (sum, item) => sum + item.price * item.quantity,
-    0
-)
+const subtotal = selectSubtotal(state);
+const tipAmount = selectTipAmount(state);
+const total = selectTotal(state);
 
+
+// move the calculations to a selector
+//   const subtotal = items.reduce(
+//     (sum, item) => sum + item.price * item.quantity,
+//     0
+// )
 //   let subtotal = 0;
 //   for (const item of items) {
 //     subtotal += item.quantity * item.price
 //   }
-
-  const tipAmount = subtotal * (state.tipPercentage / 100);
-
-  const total = subtotal + tipAmount;
+//   const tipAmount = subtotal * (state.tipPercentage / 100);
+//   const total = subtotal + tipAmount;
 
 return {
     subtotal,
