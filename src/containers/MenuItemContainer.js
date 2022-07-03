ba75ed436d471be2436dbc1313d56bf63bd5b6d3
Aaron Davis <108478438+arndvs@users.noreply.github.com>
2022-07-03 08:40:44 -0700

    Add selector for getting one item and it's total

--- src/containers/MenuItemContainer.js
+++ src/containers/MenuItemContainer.js
@@ -1,13 +1,28 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import MenuItem from '../components/MenuItem';
-import { removeItem, updatePrice, updateQuantity } from "../store/items/actions";
+
+import {
+    removeItem,
+    updatePrice,
+    updateQuantity
+} from "../store/items/actions";
+
+import { selectItemTotal } from "../store/items/selectors";
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        total: selectItemTotal(state, ownProps)
+    }
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
       remove: () => dispatch(removeItem(ownProps.uuid)),
       updatePrice: (price) => dispatch(updatePrice(ownProps.uuid, price)),
       updateQuantity: (quantity) => dispatch(updateQuantity(ownProps.uuid, quantity))
+    //   updateTotal: (total) => dispatch(updateQuantity(ownProps.uuid, [quantity, price]))
     };
   };
 
-export const MenuItemContainer = connect(null, mapDispatchToProps)(MenuItem);
+export const MenuItemContainer = connect(mapStateToProps, mapDispatchToProps)(MenuItem);
